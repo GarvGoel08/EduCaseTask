b5aaa421b28595a3d8297d87c5cb68d547c9c554
Garv Goel <garvgoel2927@gmail.com>
2024-08-22 18:50:32 +0530

    Added working msg

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const Routes = require('./route');
 app.use(express.json());
 app.use('/api', Routes);
 
+app.get('/', (req, res) => {
+    res.send("Welcome to the School API. Hit /api/addSchool to add a school and /api/listSchools to list schools.");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
--- route.js
+++ route.js
@@ -2,6 +2,9 @@ const { Router } = require('express');
 const router = Router();
 const { addSchool, listSchools } = require('./controller');
 
+router.get('/', (req, res) => {
+    res.send("Welcome to the School API. Hit /addSchool to add a school and /listSchools to list schools.");
+});
 router.post('/addSchool', addSchool);
 router.get('/listSchools', listSchools);
 
